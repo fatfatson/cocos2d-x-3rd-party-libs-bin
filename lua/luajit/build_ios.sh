88f8f5998f33423c0b4ae830206e297b02b576d9
wellbye <wellbye@163.com>
2015-06-19 15:58:04 +0800

    调整ios luajit编译脚本

--- lua/luajit/build_ios.sh
+++ lua/luajit/build_ios.sh
@@ -1,56 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIPO="xcrun -sdk iphoneos lipo"
 STRIP="xcrun -sdk iphoneos strip"
 
 SRCDIR=$DIR/src
 DESTDIR=$DIR/prebuilt/ios
-IXCODE=`xcode-select -print-path`
-ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
-INFOPLIST_PATH=$IXCODE/Platforms/iPhoneOS.platform/version.plist
-BUNDLE_ID=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "${INFOPLIST_PATH}")
-ISDKVER=iPhoneOS${BUNDLE_ID}.sdk
-echo $ISDKVER
 
-if [ "${ISDKVER}" = "iPhoneOS8.0.sdk" ] || [ "${ISDKVER}" = "iPhoneOS8.1.sdk" ]; then
-ISDKP=$IXCODE/usr/bin/
-else
+ISDK=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
 ISDKP=$ISDK/usr/bin/
-fi
+ISYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS8.3.sdk/
 
 rm "$DESTDIR"/*.a
 cd $SRCDIR
 
 make clean
-ISDKF="-arch arm64 -isysroot $ISDK/SDKs/$ISDKVER"
-if [ ${ISDKVER} = "iPhoneOS8.0.sdk" ] || [ ${ISDKVER} = "iPhoneOS8.1.sdk" ]; then
-make HOST_CC="gcc -m32 -arch x86_64" TARGET_FLAGS="$ISDKF" TARGET=arm64 TARGET_SYS=iOS
-else
+ISDKF="-arch arm64 -isysroot $ISYSROOT"
 make HOST_CC="gcc -m32 -arch x86_64" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-arm64.a
 
 make clean
-ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER"
-if [ ${ISDKVER} = "iPhoneOS8.0.sdk" ] || [ ${ISDKVER} = "iPhoneOS8.1.sdk" ]; then
-make HOST_CC="gcc -m32 -arch i386" TARGET_FLAGS="$ISDKF" TARGET=arm TARGET_SYS=iOS
-else
+ISDKF="-arch armv7 -isysroot $ISYSROOT"
 make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7.a
 
 make clean
-ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER"
-if [ ${ISDKVER} = "iPhoneOS8.0.sdk" ] || [ ${ISDKVER} = "iPhoneOS8.1.sdk" ]; then
-make HOST_CC="gcc -m32 -arch i386" TARGET_FLAGS="$ISDKF" TARGET=arm TARGET_SYS=iOS
-else
+ISDKF="-arch armv7s -isysroot $ISYSROOT"
 make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7s.a
-
-make clean
-make CC="gcc -m32 -arch i386" clean all
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-i386.a
 
 make clean
 make CC="gcc -m32 -arch x86_64" clean all
